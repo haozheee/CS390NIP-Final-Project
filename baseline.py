9825cb7d25a311c6fed51a2554f23962ad01617b
Haozhe Zhou <zhou929@purdue.edu>
2020-11-19 02:17:39 -0500

    model&training

--- baseline.py
+++ baseline.py
@@ -11,6 +11,101 @@ from PIL import Image
 import pickle
 
 
+# global variables
+
+# I put it outside the function because this needs to be referenced in training. Haozhe 11/09/20
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+top_k = 5000
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                  oov_token="<unk>",
+                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+VOCAB_SIZE = top_k+1
+
+
+
+class EncoderModel(tf.keras.Model):
+    def __init__(self, image_embedding_dim):
+        super(EncoderModel, self).__init__()
+
+        # encoder layers
+        self.encoderFC = tf.keras.Dense(image_embedding_dim)
+
+    '''
+    Input: 
+        image_feature_vector: image features extracted from the last pretrained CNN layer, (batch_size, 64)
+    Output: 
+        embedding: encoded vector of image features, which will be passed into the sequence model in the decoder (LSTM)
+    '''
+    def call(self, image_feature_vector):
+        x = self.fc(image_feature_vector)
+        x = tf.nn.relu(x)
+        return x
+
+
+class DecoderModel(tf.keras.Model):
+    def __init__(self, pred_embedding_dim, hidden_dim, vocab_size):
+        super(DecoderModel, self).__init__()
+
+        self.pred_embedding_dim = pred_embedding_dim
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+
+        # attention layers
+        self.attendImage = tf.keras.layers.Dense(hidden_dim)
+        self.attendHidden = tf.keras.layers.Dense(hidden_dim)
+        self.attendDense = tf.keras.layers.Dense(1)
+
+        # decoder layers
+        self.embeddingLayer = tf.keras.Embedding(vocab_size, pred_embedding_dim)
+        self.decoderGRU = tf.keras.layers.GRU(hidden_dim)
+        self.decoderFC1 = tf.keras.Dense(hidden_dim)
+        self.decoderFC2 = tf.keras.Dense(vocab_size)
+
+    '''
+       Input: 
+           image_embedding: the output from the encoder. This does not vary with time steps
+           previous_predict: the predicted token from the decoder at the time step t-1
+           previous_hidden_state: the hidden state from the decoder GRU at the time step t-1
+       Output: 
+           predict: the predicted token for the time step t
+           hidden_state: the hidden state from the decoder GRU at the time step t
+       '''
+    def call(self, image_embedding, previous_predict, previous_hidden_state):
+
+        # A. attention mechanism:
+        # uses image_embedding and previous_hidden_state to produce a weighted context vector
+
+        hidden_with_time_axis = tf.expand_dims(previous_hidden_state, 1)
+        at_image = self.attendImage(image_embedding)
+        at_pred = self.attendHidden(previous_hidden_state)
+        context_vector = self.attendDense(tf.nn.tanh(at_image + at_pred))
+
+        # B. decoding from attended context vector:
+
+        # map the previous prediction (a word token) into embedding (a vector representation of that word)
+        # so that it can be feed into the GRU layer
+        predict_embedding = self.embeddingLayer(previous_predict)
+
+        # concatenate context_vector and predict_embedding to use as the input to GRU
+        # concat_embedding: (batch_size, 1, pred_embedding_dim + hidden_size)
+        concat_embedding = tf.concat([tf.expand_dims(context_vector, 1), predict_embedding], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        predict, hidden_state = self.decoderGRU(concat_embedding)
+
+        # shape == (batch_size, max_length, hidden_size)
+        predict = self.decoderFC1(predict)
+        # x shape == (batch_size * max_length, hidden_size)
+        predict = tf.reshape(predict, (-1, predict.shape[2]))
+
+        # output shape == (batch_size * max_length, vocab)
+        predict = self.decoderFC2(predict)
+
+        return predict, hidden_state
+
+
 def load_image(image_path):
     img = tf.io.read_file(image_path)
     img = tf.image.decode_jpeg(img, channels=3)
@@ -111,10 +206,7 @@ def preprocessData(raw):
     # Preprocess and tokenize the captions
 
     # Choose the top 5000 words from the vocabulary
-    top_k = 5000
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
-                                                      oov_token="<unk>",
-                                                      filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+
     tokenizer.fit_on_texts(train_captions)
     train_seqs = tokenizer.texts_to_sequences(train_captions)
 
@@ -161,12 +253,6 @@ def preprocessData(raw):
         cap_val.extend(img_to_cap_vector[imgv])
 
     # Create a tf.data dataset for training
-
-    BATCH_SIZE = 64
-    BUFFER_SIZE = 1000
-    embedding_dim = 256
-    units = 512
-    vocab_size = top_k + 1
     num_steps = len(img_name_train) // BATCH_SIZE
     # Shape of the vector extracted from InceptionV3 is (64, 2048)
     # These two variables represent that vector shape
@@ -187,9 +273,61 @@ def preprocessData(raw):
     return dataset
 
 
-def trainModel(train_x, train_y):
-    # TODO: complete the model
-    pass
+def trainModel(dataset, epochs=10000):
+    encoder = EncoderModel(image_embedding_dim=128)
+    decoder = DecoderModel(pred_embedding_dim=128, hidden_dim=128, vocab_size=VOCAB_SIZE)
+
+    optimizer = tf.keras.optimizers.Adam()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+
+    def loss_function(real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = loss_object(real, pred)
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+        return tf.reduce_mean(loss_)
+
+    for epoch in range(epochs):
+        total_loss = 0
+        for (batch, (image, caption)) in enumerate(dataset):
+            loss = 0
+            # use 0s as the initial hidden state to feed in the GRU
+            previous_hidden_state = tf.zeros((BATCH_SIZE, decoder.hidden_dim))
+            # use <start> as the initial predict to feed in the GRU
+            previous_decoder_predict = tf.expand_dims([tokenizer.word_index['<start>']] * caption.shape[0], 1)
+
+            with tf.GradientTape() as tape:
+                image_features = encoder.call(image)
+
+                # teacher forcing to predict each token in the caption
+                for i in range(1, caption.shape[1]):
+                    # passing the features through the decoder
+                    predict, hidden = decoder.call(image_embedding=image_features,
+                                                   previous_predict=previous_decoder_predict,
+                                                   previous_hidden_state=previous_hidden_state)
+                    loss += loss_function(caption[:, i], predict)
+
+                    # needs to expand the dimension to match the input shape of the decoder
+                    previous_decoder_predict = tf.expand_dims(caption[:, i], 1)
+
+            trainable_variables = encoder.trainable_variables + decoder.trainable_variables
+            gradients = tape.gradient(loss, trainable_variables)
+            optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+            total_loss = total_loss + (loss / int(caption.shape[1]))  # average over the length of caption
+
+        if epochs % 100 == 0:
+            print('Epoch {} Loss {:.4f}'.format(
+                epoch + 1, total_loss))
+        # TODO: checkpoint saving
+        '''
+        if epoch % 5 == 0:
+            ckpt_manager.save()
+
+        '''
+
+        return encoder, decoder
 
 
 def runModel(test_x, test_y):
@@ -201,7 +339,7 @@ def main():
     print("Image Captioning Main")
     raw = getRawData()
     dataset = preprocessData(raw)
-    model = trainModel(data_x, data_y)
+    model = trainModel(dataset)
     '''preds = runModel(data_, model)
     evalResults(data[1], preds)
 
