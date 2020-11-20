ffc6560b3d757f565a73fff94cf8421da2f65cce
Automage <pranav92000@gmail.com>
2020-11-20 05:20:18 +0400

    bare captioning in place

--- baseline.py
+++ baseline.py
@@ -306,6 +306,7 @@ def trainModel(dataset, epochs=10000):
         loss_ *= mask
         return tf.reduce_mean(loss_)
 
+    # Continue from previous epoch if needed
     for epoch in range(start_epoch, epochs):
         total_loss = 0
         for (batch, (image, caption)) in enumerate(dataset):
@@ -348,9 +349,68 @@ def trainModel(dataset, epochs=10000):
     return encoder, decoder
 
 
-def runModel(test_x, test_y):
-    # TODO: complete the model
-    pass
+def evaluate(image):
+    attention_plot = np.zeros((max_length, attention_features_shape))
+
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dims(load_image(image)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(
+        img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(
+            dec_input, features, hidden)
+
+        attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result, attention_plot
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot
+
+
+def plot_attention(image, result, attention_plot):
+    temp_image = np.array(Image.open(image))
+
+    fig = plt.figure(figsize=(10, 10))
+
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = np.resize(attention_plot[l], (8, 8))
+        ax = fig.add_subplot(len_result//2, len_result//2, l+1)
+        ax.set_title(result[l])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+
+    plt.tight_layout()
+    plt.show()
+
+
+# def runModel(test_x, test_y):
+# Run captioning on validation set
+def runModel(test, model):
+    rid = np.random.randint(0, len(img_name_val))
+    image = img_name_val[rid]
+    real_caption = ' '.join([tokenizer.index_word[i]
+                             for i in cap_val[rid] if i not in [0]])
+    result, attention_plot = evaluate(image)
+
+    print('Real Caption:', real_caption)
+    print('Prediction Caption:', ' '.join(result))
+    plot_attention(image, result, attention_plot)
 
 
 def main():
